f480b895cb6ca48fd21c61f114085625237c9fa2
djgraphics28 <darwin.ibay30@gmail.com>
2024-04-16 13:30:40 +0800

    Added Laravel Media Library

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -5,14 +5,17 @@ namespace App\Models;
 use App\Models\Workshift;
 use App\Models\BankDetail;
 use App\Models\SalaryHistory;
+use Spatie\MediaLibrary\HasMedia;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Employee extends Model
+class Employee extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
 
     protected $guarded = [];
 
