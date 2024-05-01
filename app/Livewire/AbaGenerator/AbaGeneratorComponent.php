b654798a1351791c19d813208f894b35311c2f9d
darwin ibay <darwin.i@shoredigitalinc.com>
2024-05-01 22:18:38 +0800

    added relationships and fixes to generate ABA file

--- app/Livewire/AbaGenerator/AbaGeneratorComponent.php
+++ app/Livewire/AbaGenerator/AbaGeneratorComponent.php
@@ -5,12 +5,15 @@ namespace App\Livewire\AbaGenerator;
 use Carbon\Carbon;
 use App\Models\Payslip;
 use Livewire\Component;
+use App\Models\Business;
 use App\Models\Employee;
 use App\Models\Fortnight;
 use App\Models\BankDetail;
 use App\Models\BusinessUser;
+use Livewire\Attributes\Url;
+use Livewire\Attributes\Title;
 use Illuminate\Support\Facades\Auth;
-use ABWebDevelopers\AbaGenerator\Facades\Aba;
+use ABWebDevelopers\AbaGenerator\Aba;
 
 
 class AbaGeneratorComponent extends Component
@@ -18,9 +21,10 @@ class AbaGeneratorComponent extends Component
 
     public $businessId;
     public $fortnights = [];
+    #[Url]
     public $selectedFN;
     public $transaction = [];
-
+    #[Title('ABA Generator')]
     public function render()
     {
         return view('livewire.aba-generator.aba-generator-component');
@@ -38,7 +42,6 @@ class AbaGeneratorComponent extends Component
         $this->businessId = $businessUser->business_id;
 
         $this->fortnights = Fortnight::all();
-        $this->generate();
     }
 
     public function generate()
@@ -47,87 +50,12 @@ class AbaGeneratorComponent extends Component
             'selectedFN' => 'required'
         ]);
 
-        $employees = Employee::where('business_id', $this->businessId)
-            ->whereIn('id', [4, 5])
-            ->get();
+        $employeeIds = Business::with('employees')->find($this->businessId)->employees->pluck('id');
 
-        $fortnights = Fortnight::where('id', $this->selectedFN)->first();
-
-        $total_amount = 0;
-
-
-        $now = Carbon::now();
-
-        // Aba::addFileDetails([
-        //     'bank_name' => 'CBA',
-        //     'user_name' => 'LARKIN ENTERPRISES LIMITED',
-        //     'bsb' => '088-950',
-        //     'account_number' => '700927641',
-        //     'remitter' => 'LARKIN',
-        //     'user_number' => '301500',
-        //     'description' => 'Payroll',
-        //     'process_date'  => $now->format('dmy')
-        // ]);
-
-        Aba::addFileDetails([
-            'bank_name' => 'CBA',
-            'user_name' => 'Your account name',
-            'bsb' => '062-111',
-            'account_number' => '101010101',
-            'remitter' => 'Name of remitter',
-            'user_number' => '301500',
-            'description' => 'Payroll',
-            'process_date'  => '270616'
+        return redirect()->route('aba-download', [
+            'businessId' => $this->businessId,
+            'selectedFN' => $this->selectedFN,
+            'employeeIds' => $employeeIds
         ]);
-
-        foreach ($employees as $employee) {
-            $get_payslip = Payslip::where('fortnight_id', $this->selectedFN)
-                ->where('employee_id', $employee->id)
-                ->first();
-
-            $payslip_amount  = ($get_payslip->regular + $get_payslip->overtime + $get_payslip->sunday_ot + $get_payslip->holiday_ot + $get_payslip->plp_afp_ap + $get_payslip->other) - ($get_payslip->fn_tax + $get_payslip->npf + $get_payslip->ncsl + $get_payslip->cash_adv);
-            $total_amount = $total_amount + $payslip_amount;
-
-            $get_bank = BankDetail::where('employee_id', $employee->id)
-                ->where('is_active', 1)
-                ->first();
-            $transaction = [
-                'bsb' => $get_bank->bsb_code,
-                'account_number' => $get_bank->account_number,
-                'account_name'  => $get_bank->account_name,
-                'reference' => $fortnights->code,
-                'transaction_code'  => '53',
-                'amount' => $payslip_amount,
-            ];
-            // Aba::addTransaction($transaction);
-        }
-
-        $transactions = [
-            [
-                'bsb' => '111-111', // bsb with hyphen
-                'account_number' => '999999999',
-                'account_name'  => 'Jhon doe',
-                'reference' => 'Payroll number',
-                'transaction_code'  => '53',
-                'amount' => '250.87'
-            ],
-            [
-                'bsb' => '222-222', // bsb with hyphen
-                'account_number' => '888888888',
-                'account_name'  => 'Foo Bar',
-                'reference' => 'Rent',
-                'transaction_code'  => '50',
-                'amount' => '300'
-            ]
-        ];
-
-        foreach ($transactions as $transaction) {
-            Aba::addTransaction($transaction);
-        }
-
-
-        Aba::generate();
-
-        Aba::download();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Livewire\Asset\AssetComponent;
 use App\Livewire\Tax\TaxTablesComponent;
 use App\Livewire\Asset\AssetTypeComponent;
 use App\Livewire\Leave\LeaveTypeComponent;
+use App\Livewire\Nasfund\NasfundComponent;
 use App\Livewire\Payroll\PayslipComponent;
 use App\Livewire\Loan\LoanRequestComponent;
 use App\Livewire\Business\BusinessComponent;
@@ -18,12 +19,14 @@ use App\Http\Controllers\ApproveLeaveRequest;
 use App\Livewire\Holiday\SetHolidayComponent;
 use App\Livewire\Leave\LeaveRequestComponent;
 use App\Livewire\Workshift\WorkshiftComponent;
+use App\Http\Controllers\AbaGenerateController;
 use App\Livewire\Department\DepartmentComponent;
 use App\Livewire\Employee\EmployeeInfoComponent;
 use App\Livewire\Designation\DesignationComponent;
 use App\Livewire\Employee\CreateEmployeeComponent;
 use App\Livewire\Employee\ImportEmployeeComponent;
 use App\Livewire\Attendance\AttendanceLogComponent;
+use App\Livewire\AbaGenerator\AbaGeneratorComponent;
 use App\Livewire\CompanyDetails\BankDetailsComponent;
 use App\Livewire\Attendance\AttendanceImportComponent;
 use App\Livewire\EmailTemplate\EmailTemplateComponent;
@@ -33,8 +36,6 @@ use App\Livewire\EmployeeStatus\EmployeeStatusComponent;
 use App\Livewire\Attendance\AttendanceAdjustmentComponent;
 use App\Livewire\EmailTemplate\EmailTemplateTypeComponent;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Livewire\AbaGenerator\AbaGeneratorComponent;
-use App\Livewire\Nasfund\NasfundComponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -133,5 +134,6 @@ Route::get('/leave-request/success', [ApproveLeaveRequest::class, 'success'])->n
 
 // Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
 
+Route::get('/aba-download/{businessId}/{selectedFN}/{employeeIds}', [AbaGenerateController::class, 'generate'])->name('aba-download');
 // Route::post('login', [AuthenticatedSessionController::class, 'store']);
 require __DIR__ . '/auth.php';
